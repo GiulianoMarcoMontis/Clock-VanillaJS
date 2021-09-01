66a3fb933124d40a0c7a254899bc498608c8650b
GiulianoMarcoMontis <giulianomarco.montis@gmail.com>
2021-09-01 09:13:18 +0200

    added style and logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
-// Any code you will write for your website (in the future!) should go here
+const hourLine = document.querySelector('[data-hr-line]');
+const minuteLine = document.querySelector('[data-min-line]');
+const secondLine = document.querySelector('[data-sec-line]');
+
+const setClock = () => {
+  const currentDate = new Date();
+
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondLine, secondsRatio);
+  setRotation(minuteLine, minutesRatio);
+  setRotation(hourLine, hoursRatio);
+};
+
+const setRotation = (element, rotationRatio) => {
+  element.style.setProperty('--rotation', rotationRatio * 360);
+};
+
+setInterval(setClock, 1000);
+
+setClock();
